68389f8495f7f29476723f336146bcc1c295101d
anito <axelnitzschner@mac.com>
2020-11-03 13:34:35 +0100

    2

--- src/Controller/Api/UsersController.php
+++ src/Controller/Api/UsersController.php
@@ -2,6 +2,7 @@
 namespace App\Controller\Api;
 
 use Cake\Event\Event;
+use Cake\Http\Exception\ForbiddenException;
 use Cake\Http\Exception\UnauthorizedException;
 use Cake\Utility\Security;
 use Firebase\JWT\JWT;
@@ -42,11 +43,12 @@ class UsersController extends AppController
 
     public function add() {
         $this->Crud->on('afterSave', function(Event $event) {
-            if ($event->subject->created) {
+            if ($event->getSubject()->created) {
                 $this->set('data', [
-                    'id' => $event->subject->entity->id,
+                    'id' => $event->getSubject()->id,
+                    'message' => 'User created',
                     'token' => JWT::encode([
-                        'sub' => $event->subject->entity->id,
+                        'sub' => $event->getSubject()->id,
                         'exp' =>  time() + 604800 // (sec) 604800/60/60/24 = 7 days
                     ],
                     Security::getSalt())
@@ -58,9 +60,25 @@ class UsersController extends AppController
     }
 
     public function edit($id) {
-        // $request = $this->request->getData();
-        // Log::debug($request);
-        
+        $this->Crud->on('afterSave', function(Event $event) {
+            // Log::debug($event->getSubject());
+            if ($event->getSubject()->entity->hasErrors()) {
+                $errors = $event->getSubject()->entity->errors();
+                $key = array_key_first($errors);
+                $message = $errors[$key]['custom'];
+                throw new ForbiddenException(__($message));
+            }
+            if ($event->getSubject()->success) {
+                $message = __('User updated');
+            } else {
+                $message = __('User could not be updated');
+            }
+            $this->set('data', [
+                'message' => $message
+            ]);
+
+            $this->Crud->action()->config('serialize.data', 'data');
+        });
         return $this->Crud->execute();
     }
 
