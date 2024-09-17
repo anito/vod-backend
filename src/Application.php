a1a87b34c3de0b2ad76ae86708eb447c6e37a271
Axel Nitzschner <axelnitzschner@mac.com>
2024-09-17 19:14:48 +0200

    v2.1.2

--- src/Application.php
+++ src/Application.php
@@ -73,6 +73,18 @@ class Application extends BaseApplication implements AuthenticationServiceProvid
       }
 
       $this->addPlugin('Migrations');
+
+      /**
+       * Only try to load DebugKit in development mode
+       * Debug Kit should not be installed on a production system
+       */
+      if (Configure::read('debug')) {
+        try {
+          $this->addPlugin('DebugKit');
+        } catch (MissingPluginException $e) {
+          // Do not halt if the plugin is missing
+        }
+      }
     }
   }
 
